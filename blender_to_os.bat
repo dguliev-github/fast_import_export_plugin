9dd797ba3200564878990ac1a0f380657be8fb3a
dmigulgh <64034875+dmigulgh@users.noreply.github.com>
2022-12-11 21:35:56 +0200

    Add Windows scriptables

---
+++ blender_to_os.bat
@@ -0,0 +1,4 @@
+:: TODO - generate blender executable path, save it, and read it from the file
+pushd "C:\Users\DG\AppData\Roaming\Blender Foundation\Blender\builds\stable\blender-3.3.2+lts.bf24652e38a2\"
+popd
+"C:\Users\DG\AppData\Roaming\Blender Foundation\Blender\builds\stable\blender-3.3.2+lts.bf24652e38a2\blender.exe" --python "C:\Sync\Blender\current_addons\addons\fast_export_import\__init__.py" -- %1
